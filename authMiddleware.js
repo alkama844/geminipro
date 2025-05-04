d20ca9ee33cd04f5b0266d099088a37bea7af647
alkama844 <malkama844@gmail.com>
2025-05-04 20:08:05 +0600

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,1 +1,8 @@
-// Middleware to check user login
+// authMiddleware.js
+
+module.exports = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/login'); // Redirect to login if user is not authenticated
+  }
+  next(); // Proceed to the next middleware/route if authenticated
+};
